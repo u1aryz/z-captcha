4c63a429273afbb473f43975aedad730a50fa7d4
u1aryz <u1aryz.d@gmail.com>
2020-07-28 18:25:51 +0900

    Refactoring

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,95 +1,134 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
-import * as express from 'express';
+import { app, BrowserWindow, Config, ipcMain } from 'electron';
+import * as http from 'http';
 import * as process from 'process';
+import * as express from 'express';
 
-const proxyConfig = {
-  proxyRules: 'http://127.0.0.1:8200',
-  pacScript: '',
-  proxyBypassRules: '',
-};
+class Main {
+  private mainWindow: BrowserWindow | null = null;
 
-let mainWindow: BrowserWindow | null;
-let youtubeWindow: BrowserWindow | null;
+  private youtubeWindow: BrowserWindow | null = null;
 
-const expressApp = express();
-expressApp.get('/', async (_, res) => {
-  res.sendFile('./recaptcha.html', {
-    root: __dirname,
-  });
+  private server: http.Server | null = null;
 
-  // Disable proxy
-  mainWindow?.webContents.session.setProxy(
-    {
+  private static setProxy(win: BrowserWindow, config: Config): Promise<void> {
+    return new Promise<void>((resolve) => {
+      win.webContents.session.setProxy(config, resolve);
+    });
+  }
+
+  private static setProxyForCaptcha(win: BrowserWindow): Promise<void> {
+    return Main.setProxy(win, {
+      proxyRules: 'http://127.0.0.1:8200',
+      pacScript: '',
+      proxyBypassRules: '',
+    });
+  }
+
+  private static disableProxy(win: BrowserWindow): Promise<void> {
+    return Main.setProxy(win, {
       proxyRules: '',
       pacScript: '',
       proxyBypassRules: '',
-    },
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    () => {},
-  );
-});
-expressApp.use('/static', express.static(__dirname));
-const server = expressApp.listen('8200');
-
-const createWindow = async () => {
-  mainWindow = new BrowserWindow({
-    width: 500,
-    height: 640,
-    webPreferences: {
-      nodeIntegration: true,
-      webSecurity: false,
-    },
-  });
-
-  await mainWindow.loadURL('http://localhost:8200/static');
-};
-
-app.commandLine.appendSwitch('disable-web-security');
-app.commandLine.appendSwitch('disable-site-isolation-trials');
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  server.close();
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', async () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    await createWindow();
-  }
-});
-
-ipcMain.on('start-recaptcha', (_: unknown, arg: { url: string; sitekey: string }) => {
-  mainWindow?.webContents.session.setProxy(proxyConfig, async () => {
-    await mainWindow?.loadURL(arg.url);
-  });
-});
-
-ipcMain.on('solve-recaptcha', (_: unknown, arg: string) => {
-  console.log(arg);
-  mainWindow?.webContents.session.setProxy(proxyConfig, () => mainWindow?.reload());
-});
-
-ipcMain.on('open-youtube', async () => {
-  if (youtubeWindow == null) {
-    youtubeWindow = new BrowserWindow({
-      width: 600,
-      height: 850,
     });
-    youtubeWindow.on('closed', () => {
-      youtubeWindow = null;
-    });
-    // https://github.com/firebase/firebase-js-sdk/issues/2478
-    youtubeWindow.webContents.setUserAgent(
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) old-airport-include/1.0.0 Chrome Electron/7.1.7 Safari/537.36',
-    );
-    await youtubeWindow.loadURL(
-      'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fnext%3D%252F%26hl%3Dja%26app%3Ddesktop%26action_handle_signin%3Dtrue&hl=ja&passive=true&service=youtube&uilel=3&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
-    );
-  } else {
-    youtubeWindow.show();
   }
-});
+
+  public async setup(): Promise<void> {
+    app.commandLine.appendSwitch('disable-web-security');
+    app.commandLine.appendSwitch('disable-site-isolation-trials');
+    app.on('ready', async () => {
+      await this.createMainWindow();
+    });
+    app.on('window-all-closed', () => this.windowAllClosed());
+    app.on('activate', async () => {
+      await this.activeApp();
+    });
+
+    ipcMain.on('start-recaptcha', async (_: unknown, arg: { url: string; sitekey: string }) => {
+      await this.startRecaptcha(arg.url);
+    });
+    ipcMain.on('solve-recaptcha', async (_: unknown, arg: string) => {
+      await this.onSolveCaptcha(arg);
+    });
+    ipcMain.on('open-youtube', async () => {
+      await this.openYoutube();
+    });
+
+    const expressApp = express();
+    expressApp.get('/', async (_, res) => {
+      res.sendFile('./recaptcha.html', {
+        root: __dirname,
+      });
+
+      if (this.mainWindow) {
+        await Main.disableProxy(this.mainWindow);
+      }
+    });
+    expressApp.use('/static', express.static(__dirname));
+    this.server = expressApp.listen('8200');
+  }
+
+  private async createMainWindow(): Promise<void> {
+    this.mainWindow = new BrowserWindow({
+      width: 500,
+      height: 640,
+      webPreferences: {
+        nodeIntegration: true,
+        webSecurity: false,
+      },
+    });
+    await this.mainWindow.loadURL('http://localhost:8200/static');
+  }
+
+  private windowAllClosed(): void {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+    this.server?.close();
+  }
+
+  private async activeApp(): Promise<void> {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      await this.createMainWindow();
+    }
+  }
+
+  private async startRecaptcha(url: string): Promise<void> {
+    if (this.mainWindow) {
+      await Main.setProxyForCaptcha(this.mainWindow);
+      await this.mainWindow.loadURL(url);
+    }
+  }
+
+  private async onSolveCaptcha(token: string): Promise<void> {
+    console.log(token);
+    if (this.mainWindow) {
+      await Main.setProxyForCaptcha(this.mainWindow);
+      this.mainWindow.reload();
+    }
+  }
+
+  private async openYoutube(): Promise<void> {
+    if (this.youtubeWindow == null) {
+      this.youtubeWindow = new BrowserWindow({
+        width: 600,
+        height: 850,
+      });
+      this.youtubeWindow.on('closed', () => {
+        this.youtubeWindow = null;
+      });
+      // https://github.com/firebase/firebase-js-sdk/issues/2478
+      this.youtubeWindow.webContents.setUserAgent(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) old-airport-include/1.0.0 Chrome Electron/7.1.7 Safari/537.36',
+      );
+      await this.youtubeWindow.loadURL(
+        'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fnext%3D%252F%26hl%3Dja%26app%3Ddesktop%26action_handle_signin%3Dtrue&hl=ja&passive=true&service=youtube&uilel=3&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
+      );
+    } else {
+      this.youtubeWindow.show();
+    }
+  }
+}
+
+(async () => {
+  await new Main().setup();
+})();
